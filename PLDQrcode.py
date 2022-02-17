8870654340460d6d03e3d3ab67a21c6249be1c1f
JohnRen1211 <johnrengelojaylo@iskolarngbayan.pup.edu.ph>
2022-02-17 17:31:08 +0800

    Introduction

---
+++ PLDQrcode.py
@@ -0,0 +1,10 @@
+# Program for Qrcode reader
+# Notes:
+# Make program taht will scan qr code
+# Program should read qrcode using ebcam and save the information into text document
+# The date and time when program run should be save into text file 
+
+
+print('Qr Code reader open')
+
+# Import libraries
